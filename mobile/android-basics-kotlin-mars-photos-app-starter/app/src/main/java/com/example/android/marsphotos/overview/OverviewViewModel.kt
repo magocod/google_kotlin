3ad0bbf2cc15d4a73669dd1e12903582814ffbaa
magocod <magoyeison@gmail.com>
2021-03-27 10:33:46 -0400

    app mars-photos display photos list

--- mobile/android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ mobile/android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -21,18 +21,29 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.marsphotos.network.MarsApi
+import com.example.android.marsphotos.network.MarsPhoto
 import kotlinx.coroutines.launch
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
+//    private val _photos = MutableLiveData<MarsPhoto>()
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+    val photos: LiveData<List<MarsPhoto>> = _photos
+//    val photos: LiveData<MarsPhoto> = _photos
+
     // The internal MutableLiveData that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+//    private val _status = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the request status
-    val status: LiveData<String> = _status
+//    val status: LiveData<String> = _status
+    val status: LiveData<MarsApiStatus> = _status
+
     /**
      * Call getMarsPhotos() on init so we can display status immediately.
      */
@@ -46,11 +57,19 @@ class OverviewViewModel : ViewModel() {
      */
     private fun getMarsPhotos() {
         viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
             try {
-                val listResult = MarsApi.retrofitService.getPhotos()
-                _status.value = "Success: ${listResult.size} Mars photos retrieved"
+//                val listResult = MarsApi.retrofitService.getPhotos()
+//                _photos.value = MarsApi.retrofitService.getPhotos()[0]
+                _photos.value = MarsApi.retrofitService.getPhotos()
+//                _status.value = "Success: ${listResult.size} Mars photos retrieved"
+//                _status.value = "   First Mars image URL : ${_photos.value!!.imgSrcUrl}"
+//                _status.value = "Success: Mars properties retrieved"
+                _status.value = MarsApiStatus.DONE
             } catch (e: Exception) {
-                _status.value = "Failure: ${e.message}"
+//                _status.value = "Failure: ${e.message}"
+                _status.value = MarsApiStatus.ERROR
+                _photos.value = listOf()
             }
         }
     }
